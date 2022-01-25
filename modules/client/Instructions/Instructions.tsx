0b7c2201d70b50e6e3afbc026c821d7023637096
Sartaj <sartaj@sartaj.me>
2022-01-25 06:33:31 +0000

    add removal

--- modules/client/Instructions/Instructions.tsx
+++ modules/client/Instructions/Instructions.tsx
@@ -3,7 +3,7 @@ import { Box, Text, Heading, Container } from '@chakra-ui/react';
 
 export const Instructions = () => {
   return (
-    <Box mt="2" padding="4" margin="0" bg="gray.100" maxW="3xl">
+    <Box mt="2" padding="4" margin="0" bg="gray.100" width="100%">
       <Heading as="h2" size="md">
         Find Your Representatives
       </Heading>
